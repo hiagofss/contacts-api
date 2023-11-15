743b73451b8e628836edf8cedb8d1acf59daac8f
Hiago Souza <hiagofss98@gmail.com>
2023-11-15 19:55:12 -0300

    Add handlers

--- routes/routes.go
+++ routes/routes.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 )
 
@@ -18,5 +19,5 @@ func HandleRequest() {
 	r.HandleFunc("/contacts", controllers.CreateContact).Methods("POST")
 	r.HandleFunc("/contacts/{id}", controllers.DeleteContact).Methods("DELETE")
 	r.HandleFunc("/contacts/{id}", controllers.EditContact).Methods("PUT")
-	log.Fatal(http.ListenAndServe("localhost:8000", r))
+	log.Fatal(http.ListenAndServe("localhost:8000", handlers.CORS(handlers.AllowedOrigins([]string{"*"}), handlers.AllowedMethods([]string{"GET", "POST", "PUT", "DELETE"}))(r)))
 }
